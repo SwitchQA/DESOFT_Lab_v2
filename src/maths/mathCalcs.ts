eaa7ce096dacf087db65379793caf6d9febc490d
SwitchQA <1222637@isep.ipp.pt>
2023-03-16 22:28:07 +0000

    sooooo many changes

---
+++ src/maths/mathCalcs.ts
@@ -0,0 +1,75 @@
+/*
+let caboA: number = 10;
+let caboB: number = 15;
+let anguloC: number = 60;
+let result = Math.sqrt(caboA ^ 2 + caboB ^ 2 - 2 * caboA * caboB * Math.cos(anguloC));
+let resultArredondado:number = Number(result.toFixed(2));
+console.log(resultArredondado)
+
+
+let alturaPessoa: number = 1.8;
+let sombraEdificio: number = 30;
+let sombraPessoa: number = 5;
+let alturaEdificio: number = alturaPessoa * (sombraEdificio / sombraPessoa);
+let alturaEdifico2:number = (sombraEdificio * alturaPessoa) / sombraPessoa;
+console.log(alturaEdificio);
+console.log(alturaEdifico2);
+
+
+import { calcularCustoPintura } from "../lab02/functions";
+console.log(calcularCustoPintura(900, 5.49, 25, 60));
+
+
+import { horaChegada } from "../lab02/functions";
+console.log(horaChegada(23, 5, 4, 5));
+
+
+import { calcularTempoFinalProcessamento } from "../lab02/functions";
+
+console.log(calcularTempoFinalProcessamento(0, 0, 0, 3665));
+
+
+import { calcularMediaPesada } from "../lab02/functions";
+
+const nota1: number = 15;
+const nota2: number = 20;
+const nota3: number = 16;
+const peso1: number = 20;
+const peso2: number = 20;
+const peso3: number = 60;
+
+console.log(calcularMediaPesada(15, 20, 16, 20, 20, 60));
+
+
+import { calcularDistanciaPontos } from "../lab02/functions";
+
+let x1: number = 2;
+let x2: number = 2;
+let y1: number = 5;
+let y2: number = 7;
+
+console.log(calcularDistanciaPontos(2, 2, 5, 7));
+
+
+let area = 5;
+let aresta = Math.sqrt(area / 6);
+let volume = Math.pow(aresta, 3);
+
+console.log(Number(volume.toFixed(2)));
+
+
+let x = 4
+let y = 20
+let result = x % y
+console.log(result);
+
+
+import { calcularCustoMaisHoras } from "../lab02/functions";
+
+console.log(calcularCustoMaisHoras(2,5, 20));
+
+
+import { calcularMediaKilometros } from "../lab02/functions";
+
+console.log(calcularMediaKilometros(2,6,9,7,8));
+*/
